ed7cec360ee050147103953bf149924790ed5221
oloyiin <ilhememaliki@gmail.com>
2025-06-16 15:26:54 +0200

    front et backend nettoyé des fichier pas utiles et readme en cours

--- faas-frontend/redeploy-frontend.sh
+++ faas-frontend/redeploy-frontend.sh
@@ -3,9 +3,9 @@
 IMAGE_NAME="localhost:32000/faas-frontend:latest"
 DEPLOYMENT_FILE="frontend-deployment.yaml"
 DEPLOYMENT_NAME="faas-frontend"
-BACKEND_URL="http://backend-api-service"
+BACKEND_URL="${BACKEND_URL:-http://backend-api-service}"
 
-set -e  # stop script à la première erreur
+set -e  # Arrêter le script à la première erreur
 
 echo "Build de l'image frontend avec BACKEND_URL=$BACKEND_URL..."
 podman build --build-arg REACT_APP_BACKEND_URL=$BACKEND_URL -t $IMAGE_NAME .
@@ -19,9 +19,7 @@ microk8s kubectl apply -f $DEPLOYMENT_FILE
 echo "Redémarrage du déploiement frontend pour prendre en compte la nouvelle image..."
 microk8s kubectl rollout restart deployment $DEPLOYMENT_NAME
 
+echo "Attente que le déploiement soit prêt..."
+microk8s kubectl rollout status deployment $DEPLOYMENT_NAME --timeout=120s
+
 echo "Frontend rebuild, pushed, et redeployé avec succès."
-if [ $? -eq 0 ]; then
-  echo "Déploiement redémarré avec succès."
-else
-  echo "Erreur lors du redémarrage du déploiement."
-fi
